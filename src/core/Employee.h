f9465650ce1151fc37c4820e24643c32a8631b68
primary157 <victorgvbh@gmail.com>
2017-03-02 04:17:40 -0300

    DiagramaDeClasses PT atualizado (suporte a estoque adicionado, uso de sobrescrita de operadores implementado, implementação da biblioteca chrono feita parcialmente), iniciada inserção de informações aos cabeçalhos

--- src/core/Employee.h
+++ src/core/Employee.h
@@ -1,3 +1,26 @@
 #ifndef Employee_H_INCLUDED
 #define Employee_H_INCLUDED
+#include <string>
+#include "Person.h"
+#include "Vacation.h"
+#include "Purpose.h"
+namespace BSM{
+	enum State{ACTIVE, INACTIVE};
+	class Employee: public Person{
+		public:
+			//Constructors
+			Employee(std::string name, unsigned short salary);
+			Employee(std::string name, unsigned short salary, std::string identity, std::string phone);
+			Employee(std::string name, unsigned short salary, std::string identity, std::string phone, std::string email, Purpose e_function_performed);
+			//Methods
+		private:
+			unsigned short e_salary;
+			State e_status;
+			Purpose e_function_performed;
+			Vacation e_current_vacation;
+			Vacation e_last_vacation;
+			unsigned short e_paid;
+	};
+}
+
 #endif
